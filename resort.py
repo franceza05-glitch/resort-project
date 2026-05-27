3d79c3a5736bae373065f1f2d3ea0b44cf048858
Falong <franceza05@gmail.com>
2026-05-27 09:12:38 +0700

    update main window

--- resort.py
+++ resort.py
@@ -5,16 +5,15 @@ from datetime import datetime, timedelta
 
 class ResortGuest:
 
-    def __init__(self, name, room_number, people, price, days, check_in):
+    def __init__(self, name, room_number, people, price, days ):
 
         self.name = name
         self.room_number = room_number
         self.people = people
         self.price = price
         self.days = days
-
-        self.check_in = check_in
-        #self.check_in = datetime.now()
+       
+        self.check_in = datetime.now()
         self.check_out = self.check_in + timedelta(days=days)
 
 
@@ -62,53 +61,145 @@ def update_guest_list():
 
         guest_list.insert(tk.END, text)
 
+        
+def open_guest_window():
+
+    global name_entry
+    global room_entry
+    global people_entry
+    global price_entry
+    global days_entry
+    global guest_list
+
+    guest_window = tk.Toplevel()
+
+    guest_window.title(
+        "Guest Check-in"
+    )
+
+    guest_window.geometry(
+        "700x500"
+    )
+
+    # Name
+    tk.Label(
+        guest_window,
+        text="Customer Name"
+    ).pack()
+
+    name_entry = tk.Entry(
+        guest_window
+    )
+
+    name_entry.pack()
+
+    # Room
+    tk.Label(
+        guest_window,
+        text="Room Number"
+    ).pack()
+
+    room_entry = tk.Entry(
+        guest_window
+    )
+
+    room_entry.pack()
+
+    # People
+    tk.Label(
+        guest_window,
+        text="People"
+    ).pack()
+
+    people_entry = tk.Entry(
+        guest_window
+    )
+
+    people_entry.pack()
+
+    # Price
+    tk.Label(
+        guest_window,
+        text="Price"
+    ).pack()
+
+    price_entry = tk.Entry(
+        guest_window
+    )
+
+    price_entry.pack()
+
+    # Days
+    tk.Label(
+        guest_window,
+        text="Stay Days"
+    ).pack()
+
+    days_entry = tk.Entry(
+        guest_window
+    )
+
+    days_entry.pack()
+
+    # Button
+    add_button = tk.Button(
+        guest_window,
+        text="Add Guest",
+        command=add_guest
+    )
+
+    add_button.pack(
+        pady=10
+    )
+
+    # Listbox
+    guest_list = tk.Listbox(
+        guest_window,
+        width=100,
+        height=15
+    )
+
+    guest_list.pack(
+        pady=10
+    )
+
+    update_guest_list()
 
 # สร้างหน้าต่าง
 window = tk.Tk()
 
-window.title("Resort System")
-window.geometry("500x500")
-
-
-# Name
-tk.Label(window, text="Customer Name").pack()
-name_entry = tk.Entry(window)
-name_entry.pack()
-
-# Room
-tk.Label(window, text="Room Number").pack()
-room_entry = tk.Entry(window)
-room_entry.pack()
-
-# People
-tk.Label(window, text="People").pack()
-people_entry = tk.Entry(window)
-people_entry.pack()
-
-# Price
-tk.Label(window, text="Price").pack()
-price_entry = tk.Entry(window)
-price_entry.pack()
-
-# Days
-tk.Label(window, text="Stay Days").pack()
-days_entry = tk.Entry(window)
-days_entry.pack()
-
-
-# ปุ่มเพิ่มลูกค้า
-add_button = tk.Button(
-    window,
-    text="Add Guest",
-    command=add_guest
+window.title(
+    "Resort System"
 )
 
-add_button.pack(pady=10)
+window.geometry(
+    "400x300"
+)
 
 
-# list แสดงข้อมูลลูกค้า
-guest_list = tk.Listbox(window, width=70, height=15)
-guest_list.pack(pady=10)
+title_label = tk.Label(
+    window,
+    text="RESORT SYSTEM",
+    font=("Arial", 20)
+)
+
+title_label.pack(
+    pady=30
+)
+
+# ปุ่มเข้าพัก
+guest_button = tk.Button(
+    window,
+    text="Guest Check-in",
+    width=20,
+    height=2,
+    command=open_guest_window
+)
+
+guest_button.pack(
+    pady=10
+)
+
 
 
 # เริ่มโปรแกรม
--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-def find_numbers(start, end):
-    result = []
-    for i in range(start, end + 1):
-        if i % 7 == 0 and i % 5 != 0:
-            result.append(str(i))
-    return ','.join(result)
+# def find_numbers(start, end):
+#     result = []
+#     for i in range(start, end + 1):
+#         if i % 7 == 0 and i % 5 != 0:
+#             result.append(str(i))
+#     return ','.join(result)
 
-# Call the function with specific start and end values
-print(find_numbers(100, 200))
+# # Call the function with specific start and end values
+# print(find_numbers(100, 200))
+
+
 
